720469a91c266ce65cabcfa87c8f4288df54b5ec
Grigorii Peruntsev <gri4014@gmail.com>
2025-03-30 03:58:03 -0400

    Changed Password

--- backend/migrate-nedoma.js
+++ backend/migrate-nedoma.js
@@ -4,7 +4,7 @@ const path = require('path');
 
 const config = {
   user: process.env.POSTGRES_USER || 'postgres',
-  password: process.env.POSTGRES_PASSWORD || 'postgres',
+  password: process.env.POSTGRES_PASSWORD || 'GgvpIzikatka228!',
   host: process.env.POSTGRES_HOST || 'localhost',
   port: parseInt(process.env.POSTGRES_PORT || '5432'),
   database: process.env.POSTGRES_DB || 'nedoma_copy',
--- backend/src/config/database.ts
+++ backend/src/config/database.ts
@@ -2,7 +2,7 @@ import { Pool, PoolConfig } from 'pg';
 
 const config: PoolConfig = {
   user: process.env.POSTGRES_USER || 'postgres',
-  password: process.env.POSTGRES_PASSWORD || 'postgres',
+  password: process.env.POSTGRES_PASSWORD || 'GgvpIzikatka228!',
   host: process.env.POSTGRES_HOST || 'localhost',
   port: Number(process.env.POSTGRES_PORT) || 5432,
   database: process.env.POSTGRES_DB || 'nedoma_copy',
